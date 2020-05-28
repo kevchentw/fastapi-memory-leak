3d75897a94c8ce5e5e67dfab24482dffef964add
Kevin Chen <me@kevchentw.com>
2020-05-28 22:59:12 +0800

    compare async

--- readme.md
+++ readme.md
@@ -12,18 +12,29 @@ Thanks to @bestend for the sample code to reproduce memory leak.
 ## Sample Output
 
 ```
-Build....
-Start Server....
 Initial Mem Usage
 ==========================================
-fastapi-py37: 183.6MiB / 31.16GiB
-fastapi-py38: 187.6MiB / 31.16GiB
+fastapi-py37: 76.21MiB / 7.353GiB
+fastapi-py38: 75.86MiB / 7.353GiB
+fastapi-py37-async: 75.44MiB / 7.353GiB
+fastapi-py38-async: 75.62MiB / 7.353GiB
 ==========================================
-Run Requests....
+Run 1000 Requests....
+==========================================
+Run fastapi-py37
+real: 0m16.632s; user 0m4.748s; system 0m2.855s
+Run fastapi-py38
+real: 0m15.319s; user 0m4.750s; system 0m2.722s
+Run fastapi-py37-async
+real: 0m21.276s; user 0m4.877s; system 0m2.823s
+Run fastapi-py38-async
+real: 0m22.568s; user 0m5.218s; system 0m2.935s
+==========================================
 After 1000 Requests Mem Usage
 ==========================================
-fastapi-py37: 6.943GiB / 31.16GiB
-fastapi-py38: 386.4MiB / 31.16GiB
+fastapi-py37: 1.266GiB / 7.353GiB
+fastapi-py38: 144.8MiB / 7.353GiB
+fastapi-py37-async: 84.07MiB / 7.353GiB
+fastapi-py38-async: 83.63MiB / 7.353GiB
 ==========================================
-Cleanup....
 ```
--- test.sh
+++ test.sh
@@ -1,20 +1,30 @@
 echo "Build...."
 docker build -t fastapi-py37-mem-leak-test py3.7 >/dev/null 2>&1
 docker build -t fastapi-py38-mem-leak-test py3.8 >/dev/null 2>&1
+docker build -t fastapi-py37-async-mem-leak-test py3.7-async >/dev/null 2>&1
+docker build -t fastapi-py38-async-mem-leak-test py3.8-async >/dev/null 2>&1
 echo "Start Server...."
 docker run --name fastapi-py37 -d fastapi-py37-mem-leak-test >/dev/null 2>&1
 docker run --name fastapi-py38 -d fastapi-py38-mem-leak-test >/dev/null 2>&1
+docker run --name fastapi-py37-async -d fastapi-py37-async-mem-leak-test >/dev/null 2>&1
+docker run --name fastapi-py38-async -d fastapi-py38-async-mem-leak-test >/dev/null 2>&1
 echo "Initial Mem Usage"
 echo "=========================================="
-docker stats fastapi-py37 fastapi-py38 --no-stream --format "{{.Name}}: {{.MemUsage}}"
+docker stats fastapi-py37 fastapi-py38 fastapi-py37-async fastapi-py38-async --no-stream --format "{{.Name}}: {{.MemUsage}}"
 echo "=========================================="
 echo "Run 1000 Requests...."
-docker exec -it fastapi-py37 bash -c "for run in {1..1000}; do curl 127.0.0.1>/dev/null 2>&1; done"
-docker exec -it fastapi-py38 bash -c "for run in {1..1000}; do curl 127.0.0.1>/dev/null 2>&1; done"
+echo "Run fastapi-py37"
+docker exec -it fastapi-py37 bash -c "export TIMEFORMAT='real: %3lR; user %3lU; system %3lS' && time (for run in {1..1000}; do curl 127.0.0.1>/dev/null 2>&1; done)"
+echo "Run fastapi-py38"
+docker exec -it fastapi-py38 bash -c "export TIMEFORMAT='real: %3lR; user %3lU; system %3lS' && time (for run in {1..1000}; do curl 127.0.0.1>/dev/null 2>&1; done)"
+echo "Run fastapi-py37-async"
+docker exec -it fastapi-py37-async bash -c "export TIMEFORMAT='real: %3lR; user %3lU; system %3lS' && time (for run in {1..1000}; do curl 127.0.0.1>/dev/null 2>&1; done)"
+echo "Run fastapi-py38-async"
+docker exec -it fastapi-py38-async bash -c "export TIMEFORMAT='real: %3lR; user %3lU; system %3lS' && time (for run in {1..1000}; do curl 127.0.0.1>/dev/null 2>&1; done)"
 echo "After 1000 Requests Mem Usage"
 echo "=========================================="
-docker stats fastapi-py37 fastapi-py38 --no-stream --format "{{.Name}}: {{.MemUsage}}"
+docker stats fastapi-py37 fastapi-py38 fastapi-py37-async fastapi-py38-async --no-stream --format "{{.Name}}: {{.MemUsage}}"
 echo "=========================================="
 echo "Cleanup...."
-docker stop fastapi-py37 fastapi-py38 >/dev/null 2>&1
-docker rm fastapi-py37 fastapi-py38 >/dev/null 2>&1
+docker stop fastapi-py37 fastapi-py38 fastapi-py37-async fastapi-py38-async >/dev/null 2>&1
+docker rm fastapi-py37 fastapi-py38 fastapi-py37-async fastapi-py38-async >/dev/null 2>&1
